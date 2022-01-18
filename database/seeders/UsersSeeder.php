62795db7732a1294637c65ac7b91f376cf5ab1c4
Romadon Damad <romadon4514@gmail.com>
2022-01-18 17:13:57 +0700

    first commit

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        $data = [[
+            'name' => 'Don',
+            'email' => '622021126@tsu.ac.th',
+            'password' => Hash::make('123456'),
+            'address' => '222 satun',
+            'created_at' => Carbon::now(),
+        ]];
+        DB::table('users')->insert($data);
+    }
+}
